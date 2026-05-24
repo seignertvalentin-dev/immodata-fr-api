dbed2e36c84d3bf9ffd348f46990392a1c9598d2
seignertvalentin-dev <seignert.valentin@gmail.com>
2026-05-24 23:24:24 +0200

    Update estimation.js

--- api/estimation.js
+++ api/estimation.js
@@ -39,8 +39,7 @@ module.exports = (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Content-Type", "application/json");
 
-  const apiKey = req.headers["x-api-key"] || req.query.api_key;
-  if (!apiKey) return res.status(401).json({ error: "Clé API manquante" });
+// Auth gérée par RapidAPI
 
   const { ville, surface, type, etat } = req.query;
   if (!ville || !surface) {
